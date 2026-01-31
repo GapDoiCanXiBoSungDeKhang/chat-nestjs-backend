4b1077e7020e41ccb367125b4e0554c15bd05de7
LTN130925 <ltansang374@gmail.com>
2026-01-31 14:43:14 +0000

    tạo thư mục và mô hình notification

---
+++ src/module/notification/schema/notification.schema.ts
@@ -0,0 +1,34 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Types} from "mongoose";
+
+export type NotificationDocument = Notification & Document;
+
+@Schema({timestamps: true})
+export class Notification {
+    @Prop({ type: Types.ObjectId, ref: "User", required: true })
+    userId!: Types.ObjectId;
+
+    @Prop({
+        enum: [
+            "friend_request",
+            "friend_accepted",
+            "message",
+            "system"
+        ],
+        required: true
+    })
+    type!: string;
+
+    @Prop({type: Types.ObjectId})
+    refId?: Types.ObjectId;
+
+    @Prop({type: Object})
+    payload?: any;
+
+    @Prop({default: false})
+    isRead!: boolean;
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
+
+NotificationSchema.index({ userId: 1, isRead: 1, createdAt: -1 });
