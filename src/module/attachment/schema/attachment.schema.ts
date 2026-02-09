4a00e8ed110e45740e4a64a7bdf6227014b19d04
LTN130925 <ltansang374@gmail.com>
2026-02-09 08:30:16 +0000

    build schema attachment

---
+++ src/module/attachment/schema/attachment.schema.ts
@@ -0,0 +1,40 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Document, Types} from "mongoose";
+
+export type AttachmentDocument = Attachment & Document;
+
+@Schema({timestamps: true})
+export class Attachment {
+    @Prop({type: Types.ObjectId, ref: "Message", required: true})
+    messageId!: Types.ObjectId;
+
+    @Prop({type: Types.ObjectId, ref: "Conversation", required: true})
+    conversationId!: Types.ObjectId;
+
+    @Prop({
+        type: String,
+        enum: ["image", "video", "file", "voice"],
+        required: true
+    })
+    type!: string;
+
+    @Prop({type: String, required: true})
+    url!: string;
+
+    @Prop({type: String})
+    thumbnail?: string;
+
+    @Prop({type: String})
+    name?: string;
+
+    @Prop({type: Number})
+    size?: number;
+
+    @Prop({type: String})
+    mime?: string;
+
+    @Prop({type: Number})
+    duration?: number;
+}
+
+export const AttachmentSchema = SchemaFactory.createForClass(Attachment);
