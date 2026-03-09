9527711fe2ca31ebce3cb15d7c7a60e17ac543b4
LTN130925 <ltansang374@gmail.com>
2026-03-09 12:48:38 +0000

    build blockedUser in Module User

---
+++ src/module/user/schema/blockedUser.schema.ts
@@ -0,0 +1,18 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Types} from "mongoose";
+
+export type BlockedUserDocument = BlockedUser & Document;
+
+@Schema({timestamps: true})
+export class BlockedUser {
+    @Prop({type: Types.ObjectId, ref: "User", required: true})
+    blockerId!: Types.ObjectId;
+
+    @Prop({type: Types.ObjectId, ref: "User", required: true})
+    blockedId!: Types.ObjectId;
+}
+
+export const BlockedUserSchema = SchemaFactory.createForClass(BlockedUser);
+
+BlockedUserSchema.index({blockerId: 1, blockedId: 1}, {unique: true});
+BlockedUserSchema.index({blockedId: 1});
